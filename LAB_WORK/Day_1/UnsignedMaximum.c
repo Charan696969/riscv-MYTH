6c9f9d7ecaa93df9c6bd289c19db92e8c44d3ca6
Charan696969 <charankarthick2310596@ssn.edu.in>
2025-06-13 12:36:58 +0530

    Create UnsignedMaximum.c

---
+++ LAB_WORK/Day_1/UnsignedMaximum.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+        unsigned long long max = (unsigned long long) (pow(2,64)-1);
+        printf("Longest 64-bit unsigned number(unsigned long long) is $
+        return 0;
+}
